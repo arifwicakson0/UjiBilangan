426660841900424acb15ed7fec66b24f6bcc5361
arifwicakson0 <wicaksonoarif487@gmail.com>
2024-03-05 22:21:20 +0700

    Membenarkan kodingan

--- uji-bilangan5/uji-bilangan5.cpp
+++ uji-bilangan5/uji-bilangan5.cpp
@@ -1,32 +1,33 @@
 //Program kelulusan
 
-#include <iostrean>
+#include <iostream>
 using namespace std;
 
 int main() {
-    int nilMtk, nilBahasa, nilFisika;
-    string status;
-    float rerata;
+	int nilMtk, nilBahasa, nilFisika;
+	string status;
+	float rerata;
 
-    cout << "Nilai Matematika : " << endl;
-    cin >> nilMtk;
-    cout << "Nilai Bahasa : " << endl;
-    cin >> nilBahasa;
-    cout << "Nilai Fisika : " << endl;
-    cin >> nilFisika;
+	cout << "Nilai Matematika : " << endl;
+	cin >> nilMtk;
+	cout << "Nilai Bahasa : " << endl;
+	cin >> nilBahasa;
+	cout << "Nilai Fisika : " << endl;
+	cin >> nilFisika;
 
-    rerata = (nilBahasa + nilMtk + nilFisika) / 3;
-    
-    if (nilMtk > 70 || rerata > 60) {
-        status = "lulus";
-        if (nilFisika >= 90)
-            status = "Kelulusan Terbaik";
+	rerata = (nilBahasa + nilMtk + nilFisika) / 3;
 
-    }
-        else {
-            status = "Tidak lulus";
-    }
-            cout << "Nilai Matematika : " << nilMtk << endl;
-            cout << "Nilai Bahasa : " << nilBahasa << endl;
-            cout << "Nilai Fisika : " << nilFisika << endl;
-            cout << "Status Kelulusan : " << status;
+	if (nilMtk > 70 || rerata > 60) {
+		status = "Lulus";
+		if (nilFisika >= 90) {
+			status = "Lulus Terbaik";
+		}
+	}
+	else {
+		status = "Tidak Lulus";
+	}
+	cout << "Nilai Matematika : " << nilMtk << endl;
+	cout << "Nilai Bahasa : " << nilBahasa << endl;
+	cout << "Nilai Fisika : " << nilFisika << endl;
+	cout << "Setatus Kelulusan : " << status;
+}
